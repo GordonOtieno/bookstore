7295f8f29f8b02bcc3102d55facc02020b0b5e9f
GordonOtieno <otienogordon95@gmail.com>
2022-09-20 22:07:38 +0300

    Implement categories component functionalities, add redux hooks to add and read from store

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,3 +1,20 @@
-const Categories = () => <h1>Yet to come</h1>;
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.category);
+
+  const getStatusHandler = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <>
+      <button type="button" onClick={getStatusHandler}>Action</button>
+      <p>{categories}</p>
+    </>
+  );
+};
 
 export default Categories;
