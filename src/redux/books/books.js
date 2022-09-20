5ac858bf9aa0b07be88764438b2f032954b7f6aa
GordonOtieno <otienogordon95@gmail.com>
2022-09-20 21:42:58 +0300

    add initial state data, implement redux hooks to update UI data

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,39 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
+const books = [
+  {
+    id: '1',
+    category: 'Play',
+    title: 'An Enemy of the People',
+    author: 'Dr Stockman',
+    progress: {
+      currentChapter: 'Chapter 17',
+      completed: '52',
+    },
+  },
+  {
+    id: '2',
+    category: 'Novel',
+    title: 'The River Between',
+    author: 'Ngugi Wa Thiongo',
+    progress: {
+      currentChapter: 'Chapter 3: "A Lesson Learned"',
+      completed: '16',
+    },
+  },
+  {
+    id: '3',
+    category: 'Short Story',
+    title: 'The whale Rider',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Module Six',
+      completed: '12',
+    },
+  },
+];
+
 // add book action mathod
 export const addBook = (payload) => (
   {
@@ -15,7 +48,7 @@ export const removeBook = (id) => (
     id,
   });
 
-const bookReducer = (state = [], action) => {
+const bookReducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [
